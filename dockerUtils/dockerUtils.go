15bfbe830cbd4552f4f2a7166dc48946d0c5badf
Cesar Talledo <ctalledo@nestybox.com>
2021-01-12 23:38:01 +0000

    Reworked Docker utils so it deals with cases where a connection to Docker fails.
    The Docker utils lib typically connects to the Docker daemon to get info about
    containers.
    
    In some cases this connection can fail. In particular, when Docker containers
    are configured with "--restart" and a machine is rebooted, Docker will try to
    restart the containers but will not respond via it's API until the containers
    are restarted. See sysbox issue #184.
    
    To ensure Sysbox works properly in such cases, this change modifies the Docker
    utils so that it can deal with scenarios where Docker is not responding via it's
    API. In particular, function ContainerIsDocker() was reworked so that it can
    tell if a container is a Docker container without necessarily querying this info
    from Docker itself.
    
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- dockerUtils/dockerUtils.go
+++ dockerUtils/dockerUtils.go
@@ -7,13 +7,23 @@ package dockerUtils
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/nestybox/sysbox-libs/utils"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
+// Set to true during testing only
+var testMode = false
+
 type ContainerInfo struct {
+	Rootfs     string
 	AutoRemove bool
 }
 
@@ -23,9 +33,9 @@ type Docker struct {
 }
 
 // DockerConnect establishes a session with the Docker daemon.
-func DockerConnect() (*Docker, error) {
+func DockerConnect(timeout time.Duration) (*Docker, error) {
 
-	cli, err := client.NewEnvClient()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithTimeout(timeout))
 	if err != nil {
 		return nil, fmt.Errorf("Failed to connect to Docker API: %v", err)
 	}
@@ -81,13 +91,6 @@ func (d *Docker) ContainerGetImageID(containerID string) (string, error) {
 	return containers[0].ImageID, nil
 }
 
-// ContainerIsDocker returns true if the given container ID corresponds to a
-// Docker container.
-func (d *Docker) ContainerIsDocker(id string) bool {
-	_, err := d.ContainerGetImageID(id)
-	return err == nil
-}
-
 // ContainerGetInfo returns info for the given container. Must be called
 // after the container is created.
 func (d *Docker) ContainerGetInfo(containerID string) (*ContainerInfo, error) {
@@ -97,7 +100,86 @@ func (d *Docker) ContainerGetInfo(containerID string) (*ContainerInfo, error) {
 		return nil, err
 	}
 
+	rootfs := ""
+	if info.GraphDriver.Name == "overlay2" {
+		rootfs = info.GraphDriver.Data["MergedDir"]
+	}
+
 	return &ContainerInfo{
+		Rootfs:     rootfs,
 		AutoRemove: info.HostConfig.AutoRemove,
 	}, nil
+}
+
+// ContainerIsDocker returns true if the given container ID corresponds to a
+// Docker container. It does this by first trying to query Docker for the
+// container. If this doesn't work, it uses a heuristic based on the container's
+// rootfs.
+func ContainerIsDocker(id, rootfs string) (bool, error) {
+
+	timeout := time.Duration(500 * time.Millisecond)
+
+	docker, err := DockerConnect(timeout)
+	if err == nil {
+		defer docker.Disconnect()
+		_, err := docker.ContainerGetImageID(id)
+		return (err == nil), nil
+	}
+
+	// The connection to Docker can fail when containers are restarted
+	// automatically after reboot (i.e., containers originally launched with
+	// "--restart"); Docker won't respond until those are up. See Sysbox issue
+	// #184. In this case we determine if the container is a Docker container by
+	// examining the container's rootfs.
+
+	return isDockerRootfs(rootfs)
+}
+
+// isDockerRootfs determines if the given a container rootfs is for a Docker container.
+func isDockerRootfs(rootfs string) (bool, error) {
+
+	// Check if the container rootfs is under Docker's default data root
+	// (when in test-mode, we skip this so as to do the deeper check below)
+	if !testMode {
+		if strings.Contains(rootfs, "/var/lib/docker") {
+			return true, nil
+		}
+	}
+
+	// Check the parent dirs of the rootfs (up to 5 levels) and look for the
+	// `image, network, swarm, and containers` directories that are part of the
+	// Docker data root.
+
+	dockerDirs := []string{"image", "network", "containers", "swarm"}
+
+	searchLevels := 5
+	maxFilesPerDir := 30 // the docker data root dir has typically 10->20 subdirs in it
+	path := rootfs
+
+	for i := 0; i < searchLevels; i++ {
+		path = filepath.Dir(path)
+
+		dir, err := os.Open(path)
+		if err != nil {
+			return false, fmt.Errorf("failed to open %s: %s\n", path, err)
+		}
+
+		filenames, err := dir.Readdirnames(maxFilesPerDir)
+		if err != nil {
+			return false, fmt.Errorf("failed to read directories under %s: %s\n", path, err)
+		}
+
+		isDocker := true
+		for _, dockerDir := range dockerDirs {
+			if !utils.StringSliceContains(filenames, dockerDir) {
+				isDocker = false
+			}
+		}
+
+		if isDocker {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
