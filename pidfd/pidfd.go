9f2379ebceb5cfad56f488444391307f37e25b74
Rodny Molina <rmolina@nestybox.com>
2022-01-26 14:59:34 -0500

    Doc changes in pidfd package
    Signed-off-by: Rodny Molina <rmolina@nestybox.com>

--- pidfd/pidfd.go
+++ pidfd/pidfd.go
@@ -1,4 +1,26 @@
+//
+// Copyright 2019-2021 Nestybox, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 // Package pidfd provides pidfd_open, pidfd_getfd, pidfd_send_signal support on linux 5.6+.
+//
+// pidfd_send_signal() --> kernel 5.1+
+// pidfd_open()        --> kernel 5.3+
+// pidfd_getfd()       --> kernel 5.6+
+//
+// Sysbox is currently only using pidfd_open().
 
 package pidfd
 
